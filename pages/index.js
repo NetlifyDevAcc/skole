91cae369770f66ccff4f5ff20660590259638be7
NetlifyDevAcc <119401954+NetlifyDevAcc@users.noreply.github.com>
2023-05-15 02:24:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,17 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  <Head>
+    <title>Next.js Starter!</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
 
-      <main>
-        <input type="text" id="name" name="name">
-        <input type="text" id="password" name="password">
-      </main>
+  <main>
+    <input type="text" id="name" name="name" />
+    <input type="text" id="password" name="password" />
+  </main>
 
-      <Footer />
-    </div>
+  <Footer />
+</div>
   )
 }
