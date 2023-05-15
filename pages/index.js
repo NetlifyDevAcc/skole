90fb1f714c15fe11bd29fe7ef100263a9a6e6926
NetlifyDevAcc <119401954+NetlifyDevAcc@users.noreply.github.com>
2023-05-15 02:33:08 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,36 @@ export default function Home() {
   </Head>
 
   <main>
-    <label htmlFor="name">Name:</label>
+    <label htmlFor="name">Navn:</label>
     <input type="text" id="name" name="name" />
 
     <br />
 
-    <label htmlFor="password">Password:</label>
+    <label htmlFor="password">Kodeord:</label>
     <input type="text" id="password" name="password" />
 
     <br />
 
-    <button type="submit">Submit</button>
+    <button type="submit" id="submit-btn">Login</button>
+
+    <br />
+
+    <label htmlFor="display-text" id="display-label"></label>
   </main>
 
   <Footer />
 </div>
+<script>
+  const btn = document.getElementById('submit-btn');
+  const displayLabel = document.getElementById('display-label');
+
+  btn.addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+
+    const displayText = `Name: ${name}, Password: ${password}`;
+    displayLabel.innerText = displayText;
+  });
+</script>
   )
 }
