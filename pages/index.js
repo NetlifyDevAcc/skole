1b8a76c4e2b075d4d20302c1c30d8ec18f3f7bf9
NetlifyDevAcc <119401954+NetlifyDevAcc@users.noreply.github.com>
2023-05-15 02:34:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,17 +30,5 @@ export default function Home() {
 
   <Footer />
 </div>
-<script>
-  const btn = document.getElementById('submit-btn');
-  const displayLabel = document.getElementById('display-label');
-
-  btn.addEventListener('click', () => {
-    const name = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
-
-    const displayText = `Name: ${name}, Password: ${password}`;
-    displayLabel.innerText = displayText;
-  });
-</script>
   )
 }
